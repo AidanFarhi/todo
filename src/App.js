00297fda5b167c2b02e27981af6b726db6f92207
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-05 19:39:27 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,1 +1,20 @@
+import React from 'react'
 
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      message: 'Hello World'
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    )
+  }
+}
+
+export default App
