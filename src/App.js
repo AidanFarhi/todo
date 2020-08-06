a648ea2946d822721334e0dacaa9831b99910629
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-05 21:11:17 -0400

    check function is not working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React from 'react'
+import TodoItem from './TodoItem'
+import todoItems from './todoItems'
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      message: 'Hello World'
+      todos: todoItems
     }
+    this.check = this.check.bind(this)
+  }
+
+  check(id){
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+      return {
+        todos: updatedTodos
+      }
+    })
   }
 
   render() {
+    const todoComponents = this.state.todos.map(item => <TodoItem key={item.id} item={item} check={this.check} />)
     return (
-      <div>
-        Hello World!
+      <div className='todo-list'>
+        {todoComponents}
       </div>
     )
   }
---
+++ src/todoItems.js
@@ -0,0 +1,28 @@
+const todoItems = [
+    {
+        id:1, completed: false, text: 'Wash the car'
+    },
+    {
+        id:2, completed: false, text: 'Do the laundry'
+    },
+    {
+        id:3, completed: false, text: 'Get the kids a new bike'
+    },
+    {
+        id:4, completed: false, text: 'Learn React'
+    },
+    {
+        id:5, completed: false, text: 'Get a sweet web-dev job'
+    },
+    {
+        id:6, completed: false, text: 'Travel'
+    },
+    {
+        id:7, completed: false, text: 'Do 1000 push-ups'
+    },
+    {
+        id:8, completed: false, text: 'Resurface the roof'
+    }
+]
+
+export default todoItems
